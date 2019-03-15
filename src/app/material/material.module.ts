1739537b9630214441985979120cb3d2e771db70
cristian gil <cristianingeniero2018@hotmail.com>
2019-03-15 11:08:19 -0500

    agregando un componente toolbar de angular material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
-
+import {MatToolbarModule} from '@angular/material';
 @NgModule({
   declarations: [MaterialDemoComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports:[
+    MatToolbarModule
+  ],
 })
 export class MaterialModule { }
