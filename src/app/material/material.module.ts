e29f4fcf80c3e41c99bd8243eb3e25259002792d
cristian gil <cristianingeniero2018@hotmail.com>
2019-03-18 11:28:01 -0500

    generando componente input-fields

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
-import {MatToolbarModule,MatButtonModule,MatIconModule,MatExpansionModule} from '@angular/material';
+import {MatToolbarModule,MatButtonModule,MatIconModule,MatExpansionModule,MatInputModule} from '@angular/material';
 import { MaterialButtonComponent } from './material-button/material-button.component';
+import { MaterialInputComponent } from './material-input/material-input.component';
 @NgModule({
-  declarations: [MaterialDemoComponent, MaterialButtonComponent],
+  declarations: [MaterialDemoComponent, MaterialButtonComponent, MaterialInputComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule
   ],
   //en los exports permite usar los módulos en el proyecto
   exports:[
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule
   ],
 })
 export class MaterialModule { }
